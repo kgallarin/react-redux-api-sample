00da7ed8d97e4498e532f48b029e95788f8832f4
gallarinkevinm@gmail.com <gallarinkevinm@gmail.com>
2018-09-18 15:25:28 +0800

    last commit before heroku test deploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import Store from "./store";
 import uiThemeConfig from "./styles/MuiThemeConfig";
 
@@ -21,4 +20,3 @@ const UnsplashToyApp = () => (
 );
 
 ReactDOM.render(<UnsplashToyApp />, document.getElementById("root"));
-registerServiceWorker();
